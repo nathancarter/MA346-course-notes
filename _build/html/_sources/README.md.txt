7490af3b8750e386e746fa64a53f05450dea1a60
Nathan Carter <ncarter@bentley.edu>
2020-05-19 17:27:31 -0400

    Removing old build folder location

--- _build/html/_sources/README.md.txt
+++
@@ -1,9 +0,0 @@
-
-# MA346-course-notes
-
-Course notes for Bentley University Data Science (MA346)
-
-This repository has just begun and contains almost nothing useful yet.
-
-Come back later!
-
--- _build/jupyter_execute/intro.py
+++
@@ -1,7 +0,0 @@
-# Course Notes for Data Science (MA346)
-
-These course notes are for [Bentley University](http://www.bentley.edu)'s Data Science course (MA346) that will be taught by [Nathan Carter](http://nathancarter.github.io) in Fall 2020.
-
-```{warning}
-This course notes website is just beginning, so the content here is incomplete.  Check back later.
-```
