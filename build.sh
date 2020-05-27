06e92ad32faf534f3e891fcb639f1c2c01a8b6d6
Nathan Carter <ncarter@bentley.edu>
2020-05-27 08:32:23 -0400

    No longer rename folders in build process

--- build.sh
+++ build.sh
@@ -3,20 +3,5 @@ echo "Removing build/ folder..."
 rm -rf build
 echo "Running build..."
 jupyter-book build .
-echo "Renaming _build/_static/_sources to remove underscores..."
-mv _build build
-mv build/html/_static build/html/static
-mv build/html/_sources build/html/sources
-echo "Updating all links to remove underscores..."
-for file in build/html/*.html
-do
-    echo "    $file"
-    sed 's/_static\//\.\/static\//g' $file > $file.tmp
-    rm $file
-    mv $file.tmp $file
-    sed 's/_sources\//\.\/sources\//g' $file > $file.tmp
-    rm $file
-    mv $file.tmp $file
-done
 echo "Done."
 
