3825317436dec191b4dd85c816ebf1f30477cbd3
Nathan Carter <ncarter@bentley.edu>
2020-06-08 16:22:11 -0400

    Update course schedule table

--- _build/jupyter_execute/intro.py
+++ _build/jupyter_execute/intro.py
@@ -3,37 +3,59 @@
 These course notes are for [Bentley University](http://www.bentley.edu)'s Data Science course (MA346) that will be taught by [Nathan Carter](http://nathancarter.github.io) in Fall 2020.
 
 ```{warning}
-This course notes website is just beginning, so the content here is incomplete.  Check back later.
+This course notes website is just beginning, so the content here is incomplete.  Check back later.    
 ```
 
 ## Course schedule
 
 Each topic below will become a link to what we will cover in class that day, once I've made more progress in completing these course notes.
 
-| Week | Date in Fall 2020 | Content for the day   |
-|------|-------------------|-----------------------|
-| 1    | 9/3   | Introduction and mathematical foundations    |
-|      |       | [DataCamp Review before Week 2](big-cheat-sheet.html#before-week-2-review-of-cs230) |
-| 2    | 9/10  | Jupyter and a Python/pandas review           |
-|      |       | [DataCamp Review before Week 3](big-cheat-sheet.html#before-week-3) |
-| 3    | 9/17  | Before and after, and single-table verbs     |
-|      |       | [DataCamp Review before Week 4](big-cheat-sheet.html#before-week-4-review-of-visualization-in-cs230) |
-| 4    | 9/24  | Abstraction, Version Control, and GitHub     |
-|      |       | [DataCamp Review before Week 5](big-cheat-sheet.html#before-week-5) |
-| 5    | 10/1  | Math and Stats in Python, with Visualization |
-|      |       | [DataCamp Review before Week 6](big-cheat-sheet.html#before-week-6) |
-| 6    | 10/8  | Processing the rows of a DataFrame           |
-|      |       | (No DataCamp for next week. Finish project instead.) |
-| 7    | 10/15 | Concatenation and Merging                    |
-|      |       | [DataCamp Review before Week 8](big-cheat-sheet.html#before-week-8) |
-| 8    | 10/22 | Miscellaneous Munging Methods (ETL)          |
-|      |       | [DataCamp Review before Week 9](big-cheat-sheet.html#before-week-9) |
-| 9    | 10/29 | Dashboards                                   |
-| 10   | 11/5  | Relations as Graphs and Network Analysis     |
-| 11   | 11/12 | Relations as Matrices and Decompositions     |
-| 12   | 11/19 | Introduction to Machine Learning             |
-| 13   | 11/26 | Thanksgiving break                           |
-| 14   | 12/3  | Final Exam Review and Final Project Workshop |
+| Week | Date      | Content for the day   |
+|------|-----------|-----------------------|
+| 1    | 9/3/2020  | Introduction to data science ([notes](chapter-1-intro-to-data-science), <a href='../../_slides/chapter-1-slides.html'>slides</a>) |
+|      |           | Mathematical foundations (not yet ready) |
+|      |           | [DataCamp to review before Week 2](big-cheat-sheet.html#before-week-2-review-of-cs230) |
+|      |           | &nbsp;       |
+| 2    | 9/10/2020 | Computational notebooks (Jupyter) (not yet ready) |
+|      |           | Python review focusing on pandas and mathematical foundations (not yet ready) |
+|      |           | Functional-style code vs. imperative-style code (not yet ready) |
+|      |           | [DataCamp to review before Week 3](big-cheat-sheet.html#before-week-3) |
+|      |           | &nbsp;       |
+| 3    | 9/17/2020 | Before and after, in mathematics and communication (not yet ready) |
+|      |           | Pandas in greater depth (not yet ready) |
+|      |           | [DataCamp to review before Week 4](big-cheat-sheet.html#before-week-4-review-of-visualization-in-cs230) |
+|      |           | &nbsp;       |
+| 4    | 9/24/2020 | Abstraction in mathematics and computing (not yet ready) |
+|      |           | Version control and GitHub (not yet ready) |
+|      |           | [DataCamp to review before Week 5](big-cheat-sheet.html#before-week-5) |
+|      |           | &nbsp;       |
+| 5    | 10/1/2020 | Math and stats in Python (not yet ready) |
+|      |           | New visualization tools (not yet ready) |
+|      |           | [DataCamp to review before Week 6](big-cheat-sheet.html#before-week-6) |
+|      |           | &nbsp;       |
+| 6    | 10/8/2020 | Processing the rows of a `DataFrame` (not yet ready) |
+|      |           | (No DataCamp for next week. Finish project instead.) |
+|      |           | &nbsp;       |
+| 7    | 10/15/2020 | Concatenation and Merging (not yet ready) |
+|      |           | [DataCamp to review before Week 8](big-cheat-sheet.html#before-week-8) |
+|      |           | &nbsp;       |
+| 8    | 10/22/2020 | Miscellaneous Munging Methods (ETL) (not yet ready) |
+|      |           | [DataCamp to review before Week 9](big-cheat-sheet.html#before-week-9) |
+|      |           | &nbsp;       |
+| 9    | 10/29/2020 | Dashboards (not yet ready) |
+|      |           | &nbsp;       |
+| 10   | 11/5/2020 | Relations as graphs and network analysis (not yet ready) |
+|      |           | &nbsp;       |
+| 11   | 11/12/2020 | Relations as matrices (not yet ready) |
+|      |           | Applications of matrix decompositions (not yet ready) |
+|      |           | &nbsp;       |
+| 12   | 11/19/2020 | Introduction to machine learning (not yet ready) |
+|      |           | &nbsp;       |
+| 13   | 11/26/2020 | Thanksgiving break                           |
+|      |           | &nbsp;       |
+| 14   | 12/3/2020 | Final Exam Review |
+|      |           | Final Project Workshop |
+
 
 
 ```{toctree}
