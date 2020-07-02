a70b1ced259c78ca43e9e0f5eb325bceec171168
Nathan Carter <ncarter@bentley.edu>
2020-07-02 14:06:56 -0400

    Clean up index page and rebuild

--- _build/jupyter_execute/intro.py
+++ _build/jupyter_execute/intro.py
@@ -12,43 +12,43 @@ Each topic below will become a link to what we will cover in class that day, onc
 
 | Week | Date      | Content for the day   |
 |------|-----------|-----------------------|
-| 1    | 9/3/2020  | Introduction to data science ([notes](chapter-1-intro-to-data-science), <a href='../../_slides/chapter-1-slides.html'>slides</a>) |
-|      |           | Mathematical foundations ([notes](chapter-2-mathematical-foundations), <a href='../../_slides/chapter-2-slides.html'>slides</a>) |
+| 1    | 9/3/2020  | Chapter 1: Introduction to data science ([notes](chapter-1-intro-to-data-science), <a href='../../_slides/chapter-1-slides.html'>slides</a>) |
+|      |           | Chapter 2: Mathematical foundations ([notes](chapter-2-mathematical-foundations), <a href='../../_slides/chapter-2-slides.html'>slides</a>) |
 |      |           | [DataCamp to review before Week 2](big-cheat-sheet.html#before-week-2-review-of-cs230) |
 |      |           | &nbsp;       |
-| 2    | 9/10/2020 | Computational notebooks (Jupyter) ([notes](chapter-3-jupyter), <a href='../../_slides/chapter-3-slides.html'>slides</a>) |
-|      |           | Python review focusing on pandas and mathematical foundations ([notes](chapter-4-review-of-python-and-pandas), no slides) |
+| 2    | 9/10/2020 | Chapter 3: Computational notebooks (Jupyter) ([notes](chapter-3-jupyter), <a href='../../_slides/chapter-3-slides.html'>slides</a>) |
+|      |           | Chapter 4: Python review focusing on pandas and mathematical foundations ([notes](chapter-4-review-of-python-and-pandas), no slides) |
 |      |           | [DataCamp to review before Week 3](big-cheat-sheet.html#before-week-3) |
 |      |           | &nbsp;       |
-| 3    | 9/17/2020 | Before and after, in mathematics and communication ([notes](chapter-5-before-and-after), <a href='../../_slides/chapter-5-slides.html'>slides</a>) |
-|      |           | Pandas single-table verbs ([notes](chapter-6-single-table-verbs), <a href='../../_slides/chapter-6-slides.html'>slides</a>) |
+| 3    | 9/17/2020 | Chapter 5: Before and after, in mathematics and communication ([notes](chapter-5-before-and-after), <a href='../../_slides/chapter-5-slides.html'>slides</a>) |
+|      |           | Chapter 6: Pandas single-table verbs ([notes](chapter-6-single-table-verbs), <a href='../../_slides/chapter-6-slides.html'>slides</a>) |
 |      |           | [DataCamp to review before Week 4](big-cheat-sheet.html#before-week-4-review-of-visualization-in-cs230) |
 |      |           | &nbsp;       |
-| 4    | 9/24/2020 | Abstraction in mathematics and computing (not yet ready) |
-|      |           | Version control and GitHub (not yet ready) |
+| 4    | 9/24/2020 | Chapter 7: Abstraction in mathematics and computing (not yet ready) |
+|      |           | Chapter 8: Version control and GitHub (not yet ready) |
 |      |           | [DataCamp to review before Week 5](big-cheat-sheet.html#before-week-5) |
 |      |           | &nbsp;       |
-| 5    | 10/1/2020 | Math and stats in Python (not yet ready) |
-|      |           | New visualization tools (not yet ready) |
+| 5    | 10/1/2020 | Chapter 9: Math and stats in Python (not yet ready) |
+|      |           | Chapter 10: New visualization tools (not yet ready) |
 |      |           | [DataCamp to review before Week 6](big-cheat-sheet.html#before-week-6) |
 |      |           | &nbsp;       |
-| 6    | 10/8/2020 | Processing the rows of a `DataFrame` (not yet ready) |
+| 6    | 10/8/2020 | Chapter 11: Processing the rows of a `DataFrame` (not yet ready) |
 |      |           | (No DataCamp for next week. Finish project instead.) |
 |      |           | &nbsp;       |
-| 7    | 10/15/2020 | Concatenation and Merging (not yet ready) |
+| 7    | 10/15/2020 | Chapter 12: Concatenation and Merging (not yet ready) |
 |      |           | [DataCamp to review before Week 8](big-cheat-sheet.html#before-week-8) |
 |      |           | &nbsp;       |
-| 8    | 10/22/2020 | Miscellaneous Munging Methods (ETL) (not yet ready) |
+| 8    | 10/22/2020 | Chapter 13: Miscellaneous Munging Methods (ETL) (not yet ready) |
 |      |           | [DataCamp to review before Week 9](big-cheat-sheet.html#before-week-9) |
 |      |           | &nbsp;       |
-| 9    | 10/29/2020 | Dashboards (not yet ready) |
+| 9    | 10/29/2020 | Chapter 14: Dashboards (not yet ready) |
 |      |           | &nbsp;       |
-| 10   | 11/5/2020 | Relations as graphs and network analysis (not yet ready) |
+| 10   | 11/5/2020 | Chapter 15: Relations as graphs and network analysis (not yet ready) |
 |      |           | &nbsp;       |
-| 11   | 11/12/2020 | Relations as matrices (not yet ready) |
+| 11   | 11/12/2020 | Chapter 16: Relations as matrices (not yet ready) |
 |      |           | Applications of matrix decompositions (not yet ready) |
 |      |           | &nbsp;       |
-| 12   | 11/19/2020 | Introduction to machine learning (not yet ready) |
+| 12   | 11/19/2020 | Chapter 17: Introduction to machine learning (not yet ready) |
 |      |           | &nbsp;       |
 | 13   | 11/26/2020 | Thanksgiving break                           |
 |      |           | &nbsp;       |
