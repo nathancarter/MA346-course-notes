4d8fbbe0069fe344373c0f6781850563f665bb52
Nathan Carter <ncarter@bentley.edu>
2020-07-24 11:55:15 -0400

    Add support for building a PDF of the notes

--- html-build.sh
+++ html-build.sh
@@ -1,6 +1,6 @@
 
 echo "Removing _build/ folder..."
-rm -rf _build
+rm -rf _build/html
 echo "Running build..."
 jupyter-book build .
 echo "Done."
---
+++ pdf-build.sh
@@ -0,0 +1,9 @@
+
+echo "Removing _build/ folder..."
+rm -rf _build/latex
+echo "Running build..."
+jupyter-book build . --builder pdflatex
+echo "Done."
+cp _build/latex/book.pdf _static/MA346-course-notes.pdf
+open _static/MA346-course-notes.pdf
+
