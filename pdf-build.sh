99c4a24426f0094fb8167c2a49927dac1585c435
Nathan Carter <ncarter@bentley.edu>
2021-05-11 11:50:54 -0400

    Get PDF builds working in dev container

--- pdf-build.sh
+++ pdf-build.sh
@@ -5,5 +5,5 @@ echo "Running build..."
 jupyter-book build . --builder pdflatex
 echo "Done."
 cp _build/latex/book.pdf _static/MA346-course-notes.pdf
-open _static/MA346-course-notes.pdf
+# open _static/MA346-course-notes.pdf
 
