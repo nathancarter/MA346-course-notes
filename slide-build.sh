95d9e72342f656c4448fc0cc345afd5c21d45972
Nathan Carter <ncarter@bentley.edu>
2021-05-18 09:11:29 -0400

    Oops this belonged in last commit

--- slide-build.sh
+++ slide-build.sh
@@ -8,8 +8,9 @@ do
         no_ext="${file%.*}"
         echo "Converting $no_ext.ipynb to $no_ext.html..."
         jupyter nbconvert $no_ext.ipynb --to=slides
-        echo "Renaming to $no_ext.html..."
         mv $no_ext.slides.html $no_ext.html
+        echo "Also creating printable version..."
+        jupyter nbconvert $no_ext.ipynb --to=html --output=$no_ext-printable.html
     fi
 done
 cd ..
